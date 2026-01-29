ceddde0cc42c888a0b089e259dd44d28670e4519
non <168718969+non123a@users.noreply.github.com>
2026-01-29 16:16:55 +0700

    invicible line for player

--- ninja-platformer-main/invincible_line.gd
+++ ninja-platformer-main/invincible_line.gd
@@ -1,16 +1,14 @@
-extends Node2D
+extends Area2D
 
-@export var push_strength: float = 300.0
+@export var push_distance: float = 20.0
 var bodies_inside: Array[CharacterBody2D] = []
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		print("Entered barrier:", body.name)
 		bodies_inside.append(body)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		print("Exited barrier:", body.name)
 		bodies_inside.erase(body)
 
 func _physics_process(delta: float) -> void:
@@ -22,4 +20,5 @@ func _physics_process(delta: float) -> void:
 		if dir == 0:
 			dir = -1
 
-		character.global_position.x += dir * push_strength * delta
+		# CONTINUOUS PUSH
+		character.global_position.x += dir * push_distance * delta
