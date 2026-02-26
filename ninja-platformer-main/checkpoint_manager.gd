f6049de46770056b3dcc13b2851daebf367da8fa
non <168718969+non123a@users.noreply.github.com>
2026-02-26 23:48:18 +0700

    first optimize file from 62MB to 46MB

--- ninja-platformer-main/checkpoint_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var last_location
-var player
-
-func _ready() -> void:
-	player = get_parent().get_node("Player")
-	last_location = player.global_position
-	
--- ninja-platformer-main/spawn_level_2_start.gd
+++
@@ -1,2 +0,0 @@
-extends Node2D
-@export var spawn_id := ""
